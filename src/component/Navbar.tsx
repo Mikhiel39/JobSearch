ba17b6e76757097937b8850d3a8ca5a529fb3e87
Mikhiel <mikhielbenji9@gmail.com>
2024-06-17 22:18:08 +0530

    Changes in the navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -28,6 +28,12 @@ function Navbar(props: NavbarProps) {
     <nav className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
       <div className="flex w-full flex-wrap items-center justify-between px-7">
         <Link
+          to="/"
+          className="ml-2 text-xl text-neutral-800 dark:text-neutral-200"
+        >
+          Home
+        </Link>
+        <Link
           to={currentUser ? "/notification" : "/login"}
           className="ml-2 text-xl text-neutral-800 dark:text-neutral-200"
         >
@@ -39,6 +45,16 @@ function Navbar(props: NavbarProps) {
         >
           {props.job}
         </Link>
+        {currentUser ? null : (
+          <>
+            <Link
+              to="/sign"
+              className="ml-2 text-xl text-neutral-800 dark:text-neutral-200"
+            >
+              Sign in
+            </Link>
+          </>
+        )}
         {currentUser ? (
           <>
             <Link
@@ -50,22 +66,6 @@ function Navbar(props: NavbarProps) {
             </Link>
           </>
         ) : null}
-        {currentUser ? null : (
-          <>
-            <Link
-              to="/sign"
-              className="ml-2 text-xl text-neutral-800 dark:text-neutral-200"
-            >
-              Sign in
-            </Link>
-          </>
-        )}
-        <Link
-          to="/"
-          className="ml-2 text-xl text-neutral-800 dark:text-neutral-200"
-        >
-          Home
-        </Link>
         {/* Add your search input and button here */}
       </div>
     </nav>
